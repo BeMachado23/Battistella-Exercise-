b3dc5706112da835f5180703b059f188862ee91b
Venom Infinity <99411987+BeMachado23@users.noreply.github.com>
2022-03-18 23:17:26 -0300

    Create Exercício 4.js

---
+++ Exercício 4.js
@@ -0,0 +1,20 @@
+nome1 = "Enzo"
+nome2 = "Vênus"
+nome3 = "Jaíza"
+nome4 = "Cairé"
+nome5 = "Babu Gavassi"
+nome6 = "Bernardo"
+
+cancelou = true
+reprovou = false
+humilhou = false
+gabaritou = true
+lacrou = true
+mitou = true
+
+console.log(nome1, cancelou)
+console.log(nome2, reprovou)
+console.log(nome5, humilhou)
+console.log(nome4, gabaritou)
+console.log(nome3, lacrou)
+console.log(nome6, mitou)
